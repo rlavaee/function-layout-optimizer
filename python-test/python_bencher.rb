2f976dbfc6cfc2d1e9ab6967081769e92c0b1d95
Rahman Lavaee <rlavaee@node2x4x2a.cs.rochester.edu>
2014-08-26 14:19:27 -0400

    sens_window_results

--- python-test/python_bencher.rb
+++ python-test/python_bencher.rb
@@ -5,15 +5,16 @@ PYTHON="/u/rlavaee/usr/bin/python"
 OPTS=[".cgc",".abc"]
 SUFFIXES=[".ni",".in"]
 PIN="/p/compiler/Pin/pin-2.12-58423-gcc.4.4.7-linux/pin -t /p/compiler/Pin/pin-2.12-58423-gcc.4.4.7-linux/source/tools/Footprint/obj-intel64/dual_fp_all.so -m 2"
-LOCA_INPUTS = {"django"=>"/u/rlavaee/benchmarks/performance/bm_django.py -n 1",
-               "fastpickle"=>"/u/rlavaee/benchmarks/performance/bm_pickle.py -n 1 --use_cpickle pickle",
-               "mako"=> "/u/rlavaee/benchmarks/performance/bm_mako.py -n 5",
-               "nqueens"=>"/u/rlavaee/benchmarks/performance/bm_nqueens.py -n 1",
-               "regex_compile"=>"/u/rlavaee/benchmarks/performance/bm_regex_compile.py -n 1",
-               "slowpickle"=>"/u/rlavaee/benchmarks/performance/bm_pickle.py -n 1 pickle"}
+LOCA_INPUTS = {"django"=>"/u/rlavaee/benchmarks/performance/bm_django.py",
+               "fastpickle"=>"/u/rlavaee/benchmarks/performance/bm_pickle.py --use_cpickle pickle",
+               "mako"=> "/u/rlavaee/benchmarks/performance/bm_mako.py",
+               "nqueens"=>"/u/rlavaee/benchmarks/performance/bm_nqueens.py",
+               "regex_compile"=>"/u/rlavaee/benchmarks/performance/bm_regex_compile.py",
+               "slowpickle"=>"/u/rlavaee/benchmarks/performance/bm_pickle.py pickle"}
 LOCA_ITERATIONS = {"django"=>1, "fastpickle"=>1, "mako"=>5, "nqueens"=>1, "regex_compile"=>1, "slowpickle"=>1}
 
 AllTrainBench="django fastpickle mako nqueens regex_compile slowpickle".split(' ')
+AllTrainBench="django mako nqueens".split(' ')
 AllBench=AllTrainBench
 SAMPLE_RATES = {".cgc"=>0, ".abc"=>8, ".fabc"=>8, ".awabc"=>6, ".babc"=>6}
 
@@ -76,14 +77,14 @@ class PythonBenchmark
     FileUtils.mv("graph#{@opt}","#{@@ResultDir}/graph#{self.bench_info}#{@opt}#{@suffix}")
   end
 
-  def run_version(input,ext,version)
+  def run_version(input,ext,version,opt=nil)
     Dir.chdir(@@BenchRoot)
     if(ext==".orig")
-      output=`#{PYTHON} perf.py -v -b #{input} #{@@PythonRoot}/python2.7.orig.ref#{@suffix} #{@@PythonRoot}/python2.7#{get_suffix(ext,version)}#{@suffix}` 
-      File.open("#{@@ResultDir}/#{input}#{get_suffix(ext,version)}#{@suffix}","w") {|f| f.write(output)}
+      output=`#{PYTHON} perf.py -v -b #{input} #{@@PythonRoot}/python2.7.orig.ref#{@suffix} #{@@PythonRoot}/python2.7#{get_suffix(ext,version,opt)}#{@suffix}` 
+      File.open("#{@@ResultDir}/#{input}#{get_suffix(ext,version,opt)}#{@suffix}","w") {|f| f.write(output)}
     elsif(ext==".icc")
-      output=`#{PYTHON} perf.py -v -b #{input} #{@@PythonRoot}/python2.7.orig.ref#{@suffix} #{@@PythonRoot}/python2.7#{get_suffix(ext,version)}#{@suffix}`
-      FileUtils.mv("hw_cntrs.out", "#{@@ResultDir}/#{input}#{get_suffix(ext,version)}#{@suffix}")
+      output=`#{PYTHON} perf.py -v -b #{input} #{@@PythonRoot}/python2.7.orig.ref#{@suffix} #{@@PythonRoot}/python2.7#{get_suffix(ext,version,opt)}#{@suffix}`
+      FileUtils.mv("hw_cntrs.out", "#{@@ResultDir}/#{input}#{get_suffix(ext,version,opt)}#{@suffix}")
     end
   end
 
@@ -102,22 +103,22 @@ class PythonBenchmark
     return "#{loca_output_f}.i"
   end
 
-  def run(input,ext,version,stress=1)
+  def run(input,ext,version,opt=nil,stress=1)
     puts input
     puts ext
     puts version
+    puts stress
     Dir.chdir(@@BenchRoot)
     commands = Array.new
     proc_aff = 1
     binding = (stress > 1)?("taskset #{"%x" % proc_aff} "):("")
     stress.times.each do |i|
-      commands << "#{binding}#{@@PythonRoot}/python2.7#{get_suffix(ext,version)}#{@suffix} #{LOCA_INPUTS[input]} -n #{LOCA_ITERATIONS[input]*20}"
+      commands << "#{binding}#{@@PythonRoot}/python2.7#{get_suffix(ext,version,opt)}#{@suffix} #{LOCA_INPUTS[input]} -n #{LOCA_ITERATIONS[input]*20}"
     end
 
     pids = Array.new
     threads = Array.new
     stdouts = Array.new
-    puts stress
 
     t1= Time.new.to_f
     commands.each do |command|
@@ -146,22 +147,23 @@ class PythonBenchmark
             end
           end
         end
+				FileUtils.rm("hw_cntrs_#{pid}.out")
       end
 
-      File.open("#{@@ResultDir}/#{input}.stress#{stress}#{get_suffix(ext,version)}#{@suffix}","w") { |f| f.write(YAML.dump(counts))}
+      File.open("#{@@ResultDir}/#{input}.stress#{stress}#{get_suffix(ext,version,opt)}#{@suffix}","w") { |f| f.write(YAML.dump(counts))}
     else
-      File.open("#{@@ResultDir}/#{input}.stress#{stress}#{get_suffix(ext,version)}#{@suffix}","w") {|f| f.write(YAML.dump(t2-t1))}
+      File.open("#{@@ResultDir}/#{input}.stress#{stress}#{get_suffix(ext,version,opt)}#{@suffix}","w") {|f| f.write(YAML.dump(t2-t1))}
     end
   end
 
-  def get_runtime(input,version,stress=1)
-    File.open("#{@@ResultDir}/#{input}.stress#{stress}#{get_suffix(".orig",version)}#{@suffix}","r") {|f| f.readline}
+  def get_runtime(input,version,opt=nil,stress=1)
+  	YAML.load_file("#{@@ResultDir}/#{input}.stress#{stress}#{get_suffix(".orig",version,opt)}#{@suffix}")
   end
 
-  def get_stress_counts(input,version,stress=1)
-    YAML.load_file("#{@@ResultDir}/#{input}.stress#{stress}#{get_suffix(".icc",version)}#{@suffix}")
+  def get_stress_counts(input,version,opt=nil,stress=1)
+    YAML.load_file("#{@@ResultDir}/#{input}.stress#{stress}#{get_suffix(".icc",version,opt)}#{@suffix}")
   end
-
+  
   def dump_stress_counts(input,stress=1)
     str = "benchmark: #{input}\n"
     all_counts = Hash.new
@@ -174,7 +176,7 @@ class PythonBenchmark
 
     ["L1_ICM","L2_ICM","TLB_IM","L2_TCM"].each do |event|
       str+= event+"\n"
-      [".ref",".cgc.test",".abc.test",".fabc.test"].each do |version|
+      [".ref",".cgc.test",".abc.test"].each do |version|
         str += "#{version}\t#{all_counts[version][event]}\n"
       end
       str += "\n"
@@ -190,11 +192,11 @@ class PythonBenchmark
       pythonbench=PythonBenchmark.new(input)
       pythonbench.mws='12'
       pythonbench.suffix=".in"
-      str += ".ref\t"+pythonbench.get_runtime(input,".ref",stress)+"\n"
-      [".cgc",".abc",".fabc"].each do |opt|
+      str += ".ref\t"+pythonbench.get_runtime(input,".ref",nil,stress)+"\n"
+      [".cgc",".abc"].each do |opt|
         pythonbench.sr=SAMPLE_RATES[opt]
         pythonbench.opt = opt
-        str += opt+"\t"+pythonbench.get_runtime(input,".test",stress)+"\n"
+        str += opt+"\t"+pythonbench.get_runtime(input,".test",opt,stress)+"\n"
       end
       str += "\n"
     end
@@ -264,11 +266,12 @@ class PythonBenchmark
   end
 
   def PythonBenchmark.run_ref(stress=1)
+		puts stress
     AllTrainBench.each do |input|
       pythonbench=PythonBenchmark.new(input)
       pythonbench.suffix=".in"
-      pythonbench.run(input,".orig",".ref",stress)
-      pythonbench.run(input,".icc",".ref",stress)
+      pythonbench.run(input,".orig",".ref",nil,stress)
+      pythonbench.run(input,".icc",".ref",nil,stress)
     end
   end
 
@@ -291,6 +294,81 @@ class PythonBenchmark
       end
     end
   end
+
+	def PythonBenchmark.run_sens_wsize(train=false,stress=1)
+		window_sizes="2 4 6 8 10 12 14 20 25 30 35 40".split(' ')
+		AllTrainBench.each do |trainbench|
+			pythonbench=PythonBenchmark.new(trainbench)
+			pythonbench.sr='8'
+			pythonbench.suffix=".in"
+			[".abc"].each do |opt|
+				pythonbench.opt=opt
+				pythonbench.mws='40'
+				pythonbench.train(opt) if (train)
+				window_sizes.each do |mws|
+					pythonbench.mws=mws
+					Dir.chdir("/u/rlavaee/benchmarks")
+					[trainbench].each do |input|
+						pythonbench.rebuild_optimized(".mws#{mws}#{opt[1..-1]}") if(train)
+						pythonbench.run(input,".orig",".test",".mws#{mws}#{opt[1..-1]}",stress)
+						pythonbench.run(input,".icc",".test",".mws#{mws}#{opt[1..-1]}",stress)
+					end
+				end
+			end
+		end
+	end
+
+	def PythonBenchmark.sens_wsize_dump_runtimes(stress=1)
+		str = String.new
+		window_sizes="2 4 6 8 10 12 14 20 25 30 35 40".split(' ')
+		str += "input\t"+window_sizes.join("\t")+"\n"
+		AllTrainBench.each do |trainbench|
+			str += trainbench+"\t"
+			pythonbench=PythonBenchmark.new(trainbench)
+			pythonbench.sr='8'
+			pythonbench.suffix=".in"
+			ref_runtime = pythonbench.get_runtime(trainbench,".ref",nil,stress)
+			[".abc"].each do |opt|
+				#str+=opt+"\t"
+				pythonbench.opt=opt
+				window_sizes.each do |mws|
+					pythonbench.mws=mws
+					str+=(1-pythonbench.get_runtime(trainbench,".test",".mws#{mws}#{opt[1..-1]}",stress).to_f/ref_runtime).to_s+"\t"
+				end
+			end
+			str+="\n"
+		end
+		str
+	end
+
+	def PythonBenchmark.sens_wsize_dump_cache_counts(stress=1)
+		str = String.new
+		window_sizes="2 4 6 8 10 12 14 20 25 30 35 40".split(' ')
+		str += "input\t"+window_sizes.join("\t")+"\n"
+		AllTrainBench.each do |trainbench|
+			str += trainbench+"\t"
+			pythonbench=PythonBenchmark.new(trainbench)
+			pythonbench.sr='8'
+			pythonbench.suffix=".in"
+			[".abc"].each do |opt|
+				#str+=opt+"\t"
+				pythonbench.opt=opt
+				window_sizes.each do |mws|
+					pythonbench.mws=mws
+					counts = pythonbench.get_stress_counts(trainbench,".test",".mws#{mws}#{opt[1..-1]}",stress)
+					test_l1 = counts["L1_ICM"]
+					test_l2 = counts["L2_ICM"]
+					test_clk =test_l1*10 + test_l2*40
+					#str+=test_clk.to_s+"\t"
+					str+=counts.to_s+"\n"
+
+				end
+			end
+			str+="\n"
+		end
+		str
+	end
+
 
 
 end
