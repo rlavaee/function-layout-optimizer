221f8e10de07d72b22ce3aab2a77b54ca823ea70
Rahman Lavaee <r.lavaee@gmail.com>
2014-08-26 14:20:24 -0400

    some commits

--- python-test/benchpython.rb
+++ python-test/benchpython.rb
@@ -2,10 +2,13 @@ require_relative 'python_bencher'
 
 stress = ARGV[0].to_i
 stress= 1 if (stress==0)
-#PythonBenchmark.rebuild_tracer(".ni")
-PythonBenchmark.run_ref(stress)
-PythonBenchmark.run_all(true,stress)
+#PythonBenchmark.rebuild_tracer(".in")
+#PythonBenchmark.rebuild_pg(".in")
+#PythonBenchmark.rebuild_regular(".in")
+#PythonBenchmark.run_pg(".in",true)
+#PythonBenchmark.run_ref(stress)
+#PythonBenchmark.run_all(true,stress)
 #PythonBenchmark.perf_count_all(stress)
 #[".cgc",".abc"].each {|opt| PythonBenchmark.build_opt(opt,true)}
-#PythonBenchmark.run_loca_all(nil,true)
+PythonBenchmark.run_loca_all(nil,false)
 #puts PythonBenchmark.dump_runtimes(stress)
